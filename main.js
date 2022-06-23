d18a1ff9c9ecca1afc325bb0afe162ab149c1ebf
mardoqueu <mdq767@gmail.com>
2022-06-23 17:47:05 -0300

    Animating Sidebar

---
+++ main.js
@@ -0,0 +1,11 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
